380851ee084e4223dbc8aba482c58a02bb083a77
zlokex <andre.hovda@gmail.com>
2016-10-26 09:15:51 +0200

    Initial commit. Implemented tab activity

---
+++ app/src/main/java/com/ahovda/android/s169964_mappe2/adapter/ViewPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.ahovda.android.s169964_mappe2.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by andre on 10/26/2016.
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    // Constructor
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
